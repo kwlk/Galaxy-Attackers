2935a10de5150cca251c73552045f707bc3bd1a7
kwlk <58482306+kwlk@users.noreply.github.com>
2020-04-09 11:47:45 +0200

    Obstacle

--- Classes/Map.py
+++ Classes/Map.py
@@ -1,7 +1,10 @@
 from secrets import randbelow
 
+from .Obstacle import Obstacle
+from .Position import Position
+
 class Map:
-    def __init__(self, x, y, barrier_botom_y, barrier_top_y, player_range_bottom_y = 0, player_range_top_y = -1):
+    def __init__(self, x, y, obstacles_speed, barrier_botom_y, barrier_top_y, player_range_bottom_y = 0, player_range_top_y = -1):
         self.x = x
         self.y = y
         self.barrier_botom_y = barrier_botom_y
@@ -11,6 +14,8 @@ class Map:
             self.player_range_top_y = barrier_botom_y - 1
         else:
             self.player_range_top_y = player_range_top_y
+        self.obstacles = self.generateObstacle()
+        self.obstacles_speed = obstacles_speed
 
     def getx(self):
         return self.x
@@ -30,6 +35,17 @@ class Map:
     def getPlayerRangeTopY(self):
         return self.player_range_top_y
 
-#Obstacle dojdzie później
+    def getObstaclesSpeed(self):
+        return self.obstacles_speed
+
     def generateObstacle(self):
-        Obstacle(randbelow(self.player_range_top_y - self.player_range_bottom_y) + self.player_range_bottom_y, self.x)
+        return Obstacle(Position(randbelow(self.player_range_top_y - self.player_range_bottom_y) + self.player_range_bottom_y, self.x),self.obstacles_speed, self)
+
+    def deleteObstacle(self, obstacle: Obstacle):
+        self.obstacles = self.generateObstacle
+
+    def speedUpObstacles(self, newSpeed = -1, speedDifference = 0):
+        if newSpeed != -1:
+            self.obstacles_speed = newSpeed
+        else:
+            self.obstacles_speed += speedDifference
